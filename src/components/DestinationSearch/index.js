6b957e628a57c9e94fd4c8f83144da1bdd547899
Mayukhdevan <mayukhdevansahu@yahoo.com>
2023-01-18 18:42:40 +0000

    Addes search feature

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -3,14 +3,31 @@ import DestinationItem from '../DestinationItem'
 import './index.css'
 
 class DestinationSearch extends Component {
+  state = {inputVal: ''}
+
+  changeInputVal = e => {
+    this.setState({inputVal: e.target.value})
+  }
+
   render() {
     const {destinationsList} = this.props
+    const {inputVal} = this.state
+    console.log(inputVal)
+    const filteredList = destinationsList.filter(eachItem =>
+      eachItem.name.toLowerCase().includes(inputVal.toLowerCase()),
+    )
 
     return (
       <div className="wrapper">
         <h1 className="main-heading">Destination Search</h1>
         <div className="input-wrapper">
-          <input type="search" placeholder="Search" className="search-input" />
+          <input
+            type="search"
+            onChange={this.changeInputVal}
+            placeholder="Search"
+            className="search-input"
+            value={inputVal}
+          />
           <img
             className="search-icon"
             src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png "
@@ -18,7 +35,7 @@ class DestinationSearch extends Component {
           />
         </div>
         <ul className="list-wrapper">
-          {destinationsList.map(eachItem => (
+          {filteredList.map(eachItem => (
             <DestinationItem key={eachItem.id} destination={eachItem} />
           ))}
         </ul>
